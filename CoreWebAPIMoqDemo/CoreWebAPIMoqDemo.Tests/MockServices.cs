e4cb6e837abec4d6f1f1a2e488aa2b7a455e6f77
Binod Mahto <binodk.mahto@gmail.com>
2022-06-27 20:41:33 +0530

    Add files via upload

---
+++ CoreWebAPIMoqDemo/CoreWebAPIMoqDemo.Tests/MockServices.cs
@@ -0,0 +1,33 @@
+﻿using Moq;
+using MoqPOC.Services;
+using System.Reflection;
+
+namespace MoqPOC.Tests
+{
+    internal class MockServices
+    {
+        public Mock<IWeatherForecastService> WeatherForecastServiceMock { get; init; } 
+
+        public MockServices()
+        {
+            WeatherForecastServiceMock = new Mock<IWeatherForecastService>();
+        }
+
+        /// <summary>
+        /// This returns the collection of all mock service's interface type and the mock object, defined here i.e. <see cref="WeatherForecastServiceMock"/>.
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<(Type, object)> GetMocks()
+        {
+            return GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                .Select(x =>
+                {
+                    var interfaceType = x.PropertyType.GetGenericArguments()[0];
+                    var value = x.GetValue(this) as Mock;
+
+                    return (interfaceType, value.Object);
+                })
+                .ToArray();
+        }
+    }
+}
