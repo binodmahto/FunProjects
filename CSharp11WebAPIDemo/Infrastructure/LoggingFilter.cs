5d48f36fb31a9389cfaf3f37b057bbd06fd23ecf
Binod Mahto <binodk.mahto@gmail.com>
2022-12-11 19:43:17 +0530

    Add files via upload

---
+++ CSharp11WebAPIDemo/Infrastructure/LoggingFilter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace CSharp11WebAPIDemo.Infrastructure
+{
+    public class LoggingFilter : IAsyncActionFilter
+    {
+        public ILogger<LoggingFilter> _logger;
+
+        public LoggingFilter(ILogger<LoggingFilter> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            _logger.LogInformation("Action starts");
+            await next();
+            _logger.LogInformation("Action ends");
+        }
+    }
+    
+}
---
+++ CSharp11WebAPIDemo/Infrastructure/MyServiceFilterAttribute.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System.Diagnostics;
+
+namespace CSharp11WebAPIDemo.Infrastructure
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
+    [DebuggerDisplay("MyServiceFilter: Type={T} Order={Order}")]
+    public class MyServiceFilterAttribute<T> : Attribute, IFilterFactory, IOrderedFilter
+    {
+
+        public int Order { get; set; }
+
+        public bool IsReusable { get; set; }
+
+        public IFilterMetadata CreateInstance(IServiceProvider serviceProvider)
+        {
+            if (serviceProvider == null)
+            {
+                throw new ArgumentNullException(nameof(serviceProvider));
+            }
+
+            var filter = (IFilterMetadata)serviceProvider.GetRequiredService(typeof(T));
+            if (filter is IFilterFactory filterFactory)
+            {
+                // Unwrap filter factories
+                filter = filterFactory.CreateInstance(serviceProvider);
+            }
+
+            return filter;
+        }
+    }
+}
