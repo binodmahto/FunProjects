bd40f946b85efb0faffe3bd6169a92622f82070d
Binod Mahto <binodk.mahto@gmail.com>
2023-05-03 19:38:49 +0530

    Add files via upload

---
+++ GraphQL/graphqlapidemo/Controllers/EmployeeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace graphqlapidemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeService _employeeService;
+        public EmployeeController(IEmployeeService employeeService) {
+            _employeeService = employeeService;
+        }
+
+        [HttpGet("~/Employees")]
+        public IEnumerable<EmployeeDetails> GetEmployees()
+        {
+            return _employeeService.GetEmployees();
+        }
+
+        [HttpGet("~/EmployeesById")]
+        public IEnumerable<EmployeeDetails> GetEmployeeById(int empId)
+        {
+            return _employeeService.GetEmployee(empId);
+        }
+
+        [HttpGet("~/EmployeesByDept")]
+        public IEnumerable<EmployeeDetails> GetEmployeesByDeprtment(int deptId)
+        {
+            return _employeeService.GetEmployeesByDepartment(deptId);
+        }
+    }
+}
